1c70d9598c9d2cec0372a50b8f1641a222b7870d
gary94746 <gary.109885@gmail.com>
2020-09-29 13:37:31 -0500

    generate: photo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PhotoModule } from './photo/photo.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
     }),
+    PhotoModule,
   ],
   controllers: [AppController],
   providers: [AppService],

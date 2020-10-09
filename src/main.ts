3d5a346aad443bae62e3ce3a6d5200984cedbece
gary94746 <gary.109885@gmail.com>
2020-10-08 21:06:45 -0500

    fix: add netlify to cors policy

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: [
+      'http://localhost:4200',
+      'https://gallant-meitner-416426.netlify.app/',
+    ],
   });
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
